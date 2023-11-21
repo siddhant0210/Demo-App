1f4e92c631ec96d9009feb1ea0112c8ab124cd3f
siddhant0210 <siddwivedi0210@g.mail.com>
2023-11-22 00:13:13 +0530

    Finished

--- app/src/main/java/com/example/demoapp/Login.kt
+++ app/src/main/java/com/example/demoapp/Login.kt
@@ -1,13 +1,47 @@
 package com.example.demoapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
 
+    private lateinit var mAuth: FirebaseAuth
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        edtEmail = findViewById(R.id.edit_email)
+        edtPassword = findViewById(R.id.edit_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email , password);
+        }
+
+    }
+
+    private fun login(email: String, password: String){
+
     }
 }
--- app/src/main/java/com/example/demoapp/SignUp.kt
+++ app/src/main/java/com/example/demoapp/SignUp.kt
@@ -1,11 +1,52 @@
 package com.example.demoapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.math.sign
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtName: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+        edtName = findViewById(R.id.edit_name)
+        edtEmail = findViewById(R.id.edit_email)
+        edtPassword = findViewById(R.id.edit_password)
+//        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email , password)
+        }
+    }
+
+    private fun signUp(user: String , password: String){
+//        //logic of creating user
+//       // mAuth.createUserWithEmailAndPassword(email, password)
+//        //    .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//
+//                } else {
+//
+//                }
+//            }
+
     }
 }
