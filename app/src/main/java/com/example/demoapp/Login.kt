9a76e16c17a80f895dd71d6d863bfa5a2665d7c2
siddhant <siddwivedi0210@gmail.com>
2023-11-21 17:52:59 +0530

    Final commit

---
+++ app/src/main/java/com/example/demoapp/Login.kt
@@ -0,0 +1,13 @@
+package com.example.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
